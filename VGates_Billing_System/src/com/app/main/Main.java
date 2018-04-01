635434b58a6a1e73297e48d872996d420dbd26ec
Asel Meemeduma <aselkamitha@gmail.com>
2018-04-01 09:31:33 +0530

    Add files via upload

---
+++ VGates_Billing_System/src/com/app/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.main;
+
+import com.app.ui.main.MainWindow;
+
+/**
+ *
+ * @author Asel
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        MainWindow mainWindow= new MainWindow();
+        mainWindow.setVisible(true);
+    }
+}
